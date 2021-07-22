c3151513e3d79193d56e85e04f3e553206157923
Som <smdtt86@gmail.com>
2021-07-22 12:26:58 +0530

    Added guard
    Added vailations and written tests

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Creating Article" do
-  scenario "User creates a new post" do
+  scenario "User creates a new Article" do
 	  visit "/"
 
 	  click_link "New Article"
@@ -11,8 +11,24 @@ RSpec.feature "Creating Article" do
 
 	  click_button "Create Article"
 
-	  expect(page).to have_content("Article has been created Created")
+	  expect(page).to have_content("Article has been created")
 		expect(page.current_path).to eq(articles_path) 
 
   end
+
+	scenario "A user fails to create a new article" do 
+		visit "/"
+
+		click_link "New Article"
+
+		fill_in "Title", with: "" 
+		fill_in "Body", with: "" 
+
+		click_button "Create Article"
+
+		expect(page).to have_content("Article has not been created") 
+		expect(page).to have_content("Title can't be blank") 
+		expect(page).to have_content("Body can't be blank")
+	end
+
 end
