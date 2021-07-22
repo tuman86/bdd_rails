14ffade9d12df8c1bf5d95cd70015316d6c74827
Som <smdtt86@gmail.com>
2021-07-22 14:05:17 +0530

    Written test cases for edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,23 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id]) 
+    if @article.update(article_params)
+      flash[:success] = "Article has been updated"
+      redirect_to @article 
+    else
+      flash.now[:danger] = "Article has not been updated"
+      render :edit
+    end
+  end
+
+
   protected
     def resource_not_found
       message = "The article you are looking for could not be found" 
