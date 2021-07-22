6852e93c0b8feea477ed4e67fdd38eea7c0f0eea
Som <smdtt86@gmail.com>
2021-07-22 15:00:04 +0530

    Test cases for delete functionality
    refactroed cod

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -20,15 +21,14 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
 
   def update
-    @article = Article.find(params[:id]) 
     if @article.update(article_params)
       flash[:success] = "Article has been updated"
       redirect_to @article 
@@ -38,6 +38,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    if @article.destroy
+      flash[:success] = "Article has been deleted."
+      redirect_to articles_path 
+    end
+  end
 
   protected
     def resource_not_found
@@ -47,6 +53,10 @@ class ArticlesController < ApplicationController
     end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :body) 
     end
---
+++ spec/features/delete_article_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+RSpec.feature "Deleting an Article" do
+
+  before do
+    @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.") 
+  end
+  scenario "A user deletes an article" do
+  
+    visit "/"
+
+    click_link @article.title 
+    click_link "Delete Article"
+
+    expect(page).to have_content("Article has been deleted")
+    expect(current_path).to eq(articles_path) 
+    
+  end
+end
