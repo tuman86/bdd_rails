77ddfbb50291cc3d38be8abc64c0177451d73fb9
Som <smdtt86@gmail.com>
2021-07-27 12:58:14 +0530

    Fixed issue in test case

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -8,7 +8,15 @@ RSpec.describe "Articles", type: :request do
 
 
   describe 'GET /articles/:id/edit' do 
-    
+    context 'with non-signed in user' do
+      before { get "/articles/#{@article.id}/edit" }
+      it "redirects to the signin page" do 
+        expect(response.status).to eq 302
+        # flash_message = "You need to sign in or sign up before continuing"
+        # expect(flash[:alert]).to eq flash_message
+      end 
+    end
+
     context 'with signed in users who are non-owners' do 
       before do
         login_as(@fred)
