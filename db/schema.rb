e0af7796c1983473cf89b9be46287919dd43d5f5
Som <smdtt86@gmail.com>
2021-07-27 10:50:52 +0530

    Defined associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_074741) do
+ActiveRecord::Schema.define(version: 2021_07_27_051731) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_07_23_074741) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Creating Article" do
+	
+	before do
+		@john = User.create(email: "john@example.com", password: "password")
+		login_as(@john) 
+	end
+
   scenario "User creates a new Article" do
 	  visit "/"
 
@@ -11,8 +17,11 @@ RSpec.feature "Creating Article" do
 
 	  click_button "Create Article"
 
+		expect(Article.last.user).to eq(@john)
+
 	  expect(page).to have_content("Article has been created")
 		expect(page.current_path).to eq(articles_path) 
+		expect(page).to have_content("Created by: #{@john.email}")
 
   end
 
